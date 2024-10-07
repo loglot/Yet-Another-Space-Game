3ddefeab5bf03803d79f9ade1f2558009c6a17d3
loglot <88983354+loglot@users.noreply.github.com>
2024-10-07 15:20:01 +0000

    worked on some collision stuff

--- system/map-player/player/player.js
+++ system/map-player/player/player.js
@@ -50,5 +50,65 @@ export class Player {
             this.game.camera.rotation += 360
         }
         this.game.camera.targetR = this.rotation
+        if(this.checkCollision(this.game.map.map[2])){
+            console.log("colide")
+        }
+        if(this.checkCollision(this.game.map.map[0])){
+            console.log("colide No Rotation")
+        }
     }
+
+    checkCollision(rect) {
+        const radians = this.degreesToRadians(rect.rotation); // get the rotation of the rectangle in radians
+        const origin = {
+            x: rect.x,
+            y: rect.y
+        }
+        const rectCorrected = {
+            x: rect.x - rect.width/2,
+            y: rect.y - rect.height/2,
+            width: rect.width,
+            height: rect.height,
+        }
+          
+        const newCirclePos = this.getRotatedPos(origin, {x: this.x, y: this.y}, radians); // rotate the circle to the same axis as the rectangle
+          
+        let closestX;
+        let closestY;
+          
+            // gets the closest x position on the rectangle to the circle
+        if (newCirclePos.x < rectCorrected.x) closestX  = rectCorrected.x;
+        else if (newCirclePos.x > rectCorrected.x + rectCorrected.width) closestX = rectCorrected.x + rectCorrected.width;
+        else closestX = newCirclePos.x;
+          
+            // gets the closest y position on the rectangle to the circle
+        if (newCirclePos.y < rectCorrected.y) closestY  = rectCorrected.y;
+        else if (newCirclePos.y > rectCorrected.y + rectCorrected.height) closestY = rectCorrected.y + rectCorrected.height;
+        else closestY = newCirclePos.y;
+          
+            // gets the distance of the closest point on the rectangle to the circle
+        const distX = Math.abs(newCirclePos.x - closestX);
+        const distY = Math.abs(newCirclePos.y - closestY);
+        const distance = Math.sqrt((distX ** 2) + (distY ** 2));
+          
+        // if the distance is less than the circle's radius, there is a collision
+        if (distance < 50) return true;
+          
+        return false;
+    }
+
+    getRotatedPos(origin, point, angle) {
+        const rotatedX = (Math.cos(angle) * (point.x - origin.x)) + (Math.sin(angle) * (point.y - origin.y)) + origin.x;
+        const rotatedY = (Math.cos(angle) * (point.y - origin.y)) - (Math.sin(angle) * (point.x - origin.x)) + origin.y;
+      
+        return {
+            x: rotatedX,
+            y: rotatedY
+        }
+    }
+
+    degreesToRadians(degrees) {
+        return degrees * Math.PI / 180;
+    }
+    
 }
